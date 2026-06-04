1dd864f3c4090efd8263a29b9b2c20408153f0d3
alumno <alumno@Instituto.local>
2026-06-04 09:07:51 -0300

    4 de junio

--- TP5/app.js
+++ TP5/app.js
@@ -1,8 +1,12 @@
-let numero1 = 25;
-let numero2 = 23;
-let resultado
+let Suma  
+let Resta 
+let Multiplicacion  
+let Division 
+let numero1 = 1
+let numero2 = 2
+ 
+Suma = numero1 + numero2;
+console.log(suma)
 
-resultado = numero1 + numero2;
-
-console.log(resultado)
-alert(resultao)
+Resta = numero2 - numero1;
+console.log(Resta)

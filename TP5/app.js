d9cff89a5891aac7efc3fa85f22b1d7badf3f3ca
alumno <51085840@ivt.edu.ar>
2026-06-11 08:17:19 -0300

    Update app.js

--- TP5/app.js
+++ TP5/app.js
@@ -6,12 +6,18 @@ let numero1 = 1
 let numero2 = 2
  
 Suma = numero1 + numero2;
-console.log(suma)
+alert("el resultado de la suma es + suma")
 
 Resta = numero2 - numero1;
-console.log(Resta)
+alert("el resultado de la resta es + resta")
 
 Multiplicacion = numero1 * numero2 * numero1
 console.log(Multiplicacion)
 
-Division = numero2
+Division = numero2/numero1
+console.log(Division)
+
+let nombrealumno = Ivanka;
+let apellido = Tomaso Sterkel;
+
+

ed194a4edc0427266f00c4d48f717e46613fb561
IvankaTomasoSterkel <51085840@ivt.edu.ar>
2026-09-01 12:56:06 -0300

    tp7

--- TP7/app.js
+++ TP7/app.js
@@ -12,56 +12,66 @@ b1.onclick = function () {
 }
 //ejercicio 2 y 3
 let nombreUsuario = 'Joaco Sanchez'
-let b2 = document.querySelector("#b2")
-let p2 = document.querySelector('#p2')
-b2.onclick = function () {
-    if ((nombreUsuario == 'nahuel') || (nombreUsuario == 'marcos'))  {
-        p2.textContent = "Bienvenido " + nombreUsuario + " ¿cómo estás?"
+let b23 = document.querySelector("#b2")
+let p23 = document.querySelector('#p2')
+b23.onclick = function () {
+    if ((nombreUsuario == 'nahuel') || (nombreUsuario == 'marcos')) {
+        p23.textContent = "Bienvenido " + nombreUsuario + " ¿cómo estás?"
     } else {
-        p2.textContent = "Bienvenido " + nombreUsuario 
+        p23.textContent = "Bienvenido " + nombreUsuario
     }
 }
 
 // ejercicio 4
 let numero = 0
 if (numero < 0) {
-    P2.textContent =  "el numero es negativo"
-}else if(numero > 0){
+    P2.textContent = "el numero es negativo"
+} else if (numero > 0) {
     P2.textContent = "el numero es positivo"
 } else {
     P2.textContent = "el numero 0"
-
+}
 
 // ejercicio 5
 b4.onclick = function () {
     let edad = 18
-    if ((edad >= 6) || (edad <= 11 )){
-         P2.textContent = "sos un niño"
-    } else if ((edad >= 12) || (edad <= 18)){
-           P2.textContent = "sos un adolecente"
+    if ((edad >= 6) || (edad <= 11)) {
+        P2.textContent = "sos un niño"
+    } else if ((edad >= 12) || (edad <= 18)) {
+        P2.textContent = "sos un adolecente"
     } else if ((edad >= 19) || (edad <= 26)) {
-           P2.textContent = "sos un joven"
-    } else if ((edad >= 27) || (edad <= 59)){
-           P2.textContent = "sos un adulto"
+        P2.textContent = "sos un joven"
+    } else if ((edad >= 27) || (edad <= 59)) {
+        P2.textContent = "sos un adulto"
     } else if (edad >= 60)
-           P2.textContent = "anciano"
-   }
-   
-   //ejercicio 6
-   let p4 = document.querySelector("#p4")
-   lte b4 = document.querySelector("#b4")
-   let dia = jueves 
-   b4.onclick = function() {
-if ((dia == lunes) || (dia == martes) || (dia == miercoles) || (dia == jueves) || (dia == viernes))
-p4.textContent = "es dia se semana" 
-} else{
-    b4.textContent = " ya es finde semana"
+        P2.textContent = "anciano"
+}
+
+//ejercicio 6
+let p4 = document.querySelector("#p4")
+let b4 = document.querySelector("#b4")
+let dia = jueves
+b4.onclick = function () {
+    if ((dia == lunes) || (dia == martes) || (dia == miercoles) || (dia == jueves) || (dia == viernes)) {
+        p4.textContent = "es dia se semana"
+    } else {
+        b4.textContent = " ya es finde semana"
+    }
 }
 
 //ejercicio 7
-let b5 = secreto 
-if ()
-   
+let b5 = document.querySelector ("#b5")
+let contraseña = document.querySelector ("#contraseña")
+
+b5.onclick = function () {
+if (contraseña == 'secreto') {
+    p5.textContent = "aceso consedido"
+} else {
+    p5.textContent = "aceso denegado"
+}
+}
+
+
 
 
 
